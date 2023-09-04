50ef473139ba70463af70ce85b1ba3d14bab9589
BimaBayuUWUUU <bimabayus7994@gmail.com>
2023-09-04 08:56:07 +0700

    Delete Junk File

--- week-01/src/hello_word_2141720119/test/hello_word_2141720119_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:hello_word_2141720119/hello_word_2141720119.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}

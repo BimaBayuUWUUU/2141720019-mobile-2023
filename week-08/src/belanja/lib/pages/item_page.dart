ecc045aff6d4e1d46162b577a8842d01bcc21cc4
BimaBayuUWUUU <bimabayus7994@gmail.com>
2023-10-22 11:56:15 +0700

    Tugas2

--- week-08/src/belanja/lib/pages/item_page.dart
+++ week-08/src/belanja/lib/pages/item_page.dart
@@ -8,33 +8,67 @@ class ItemPage extends StatelessWidget {
     final itemArgs = ModalRoute.of(context)!.settings.arguments as Item;
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.green,
         title: const Text('Toko Item'),
       ),
       body: Container(
         margin: const EdgeInsets.all(15),
-        child: Card(
-          child: Center(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  itemArgs.name.toString(),
-                  style: TextStyle(fontSize: 24),
+          child: Column(
+            children: [
+              Hero(
+                tag: itemArgs.name,
+                child: Image.asset(
+                  itemArgs.imageUrl,
+                  width: 500,
+                  height: 500,
                 ),
-                const SizedBox(width: 8), // Spasi antara "name" dan "with"
-                const Text(
-                  'with',
-                  style: TextStyle(fontSize: 18),
+              ),
+              Container(
+                padding: const EdgeInsets.all(16),
+                color: Colors.green, // Warna latar belakang hijau
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      itemArgs.name.toString(),
+                      style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 8),
+                    Row(
+                      children: [
+                        const Text(
+                          'Stok: ',
+                          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white),
+                        ),
+                        Text(
+                          itemArgs.stock.toString(),
+                          style: TextStyle(fontSize: 20, color: Colors.white),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 8),
+                    Row(
+                      children: [
+                        const Text(
+                          'Rating: ',
+                          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white),
+                        ),
+                        const Icon(
+                          Icons.star,
+                          color: Colors.yellow, // Warna bintang kuning
+                          size: 24,
+                        ),
+                        Text(
+                          itemArgs.rating.toString(),
+                          style: TextStyle(fontSize: 20, color: Colors.white),
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
-                const SizedBox(width: 8), // Spasi antara "with" dan "price"
-                Text(
-                  itemArgs.price.toString(),
-                  style: TextStyle(fontSize: 18),
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
-        ),
       ),
     );
   }

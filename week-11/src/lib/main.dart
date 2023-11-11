eab3f1ee4da1e574000c73bbcfa560707a865174
BimaBayuUWUUU <bimabayus7994@gmail.com>
2023-11-11 12:18:02 +0700

    finish_praktikum2

--- week-11/src/lib/main.dart
+++ week-11/src/lib/main.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:master_plan/models/plan.dart';
+import 'package:master_plan/providers/plan_provider.dart';
 import './views/plan_screen.dart';
 
 void main() => runApp(const MasterPlanApp());
-
 class MasterPlanApp extends StatelessWidget {
   const MasterPlanApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.purple),
-      home: const PlanScreen(),
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: PlanProvider(
+        notifier: ValueNotifier<Plan>(const Plan()),
+        child: const PlanScreen(),
+      ),
     );
   }
 }
